001aa816ee55fe6f144ab2e220bfaca730593c00
parasquid <tristan.gomez@gmail.com>
2015-12-02 16:24:48 +0800

    update context to not use "it" format

--- spec/active_record/gsheets/adapter_spec.rb
+++ spec/active_record/gsheets/adapter_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe ActiveRecord::Gsheets::Adapter do
-  context "it has a version number" do
+  context "version number" do
     Given(:version) { ActiveRecord::Gsheets::Adapter::VERSION }
     Then { version != nil }
   end
-
 end
+
