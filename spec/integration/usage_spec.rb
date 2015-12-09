9df6580006870ce357640c819a9444f3f6a400d5
parasquid <tristan.gomez@gmail.com>
2015-12-10 01:10:25 +0800

    add in some integration tests
    this is simply to automate some scenarios where i have to do a real
    connection with google spreadsheets. env vars are setup using
    dotenv.
    
    i figured someone might be interested in this as well (from setup
    to querying)

--- spec/integration/usage_spec.rb
+++ spec/integration/usage_spec.rb
@@ -17,3 +17,21 @@ describe User do
   When(:all_users) { User.all }
   Then { all_users != nil }
 end
+
+require "gsheets/session"
+require "gsheets/spread_sheet"
+require "gsheets/oauth/offline"
+require "dotenv"
+
+describe Gsheets::SpreadSheet do
+  Given!(:dotenv) { Dotenv.load }
+  Given(:client_id) { ENV["client_id"] || "client_id" }
+  Given(:client_secret) { ENV["client_secret"] || "client_secret" }
+  Given(:refresh_token) { ENV["refresh_token"] || "refresh_token" }
+  Given(:spread_sheet_id) { ENV["spread_sheet_id"] || "spread_sheet_id" }
+  Given(:authenticator)  { Gsheets::Oauth::Offline.new(client_id, client_secret) }
+  Given(:code) { authenticator.get_access_token(refresh_token: refresh_token) }
+  Given(:session) { Gsheets::Session.new(auth_code: code) }
+  Given(:ss) { Gsheets::SpreadSheet.new(session: session, id: spread_sheet_id) }
+  # Then { binding.pry }
+end
