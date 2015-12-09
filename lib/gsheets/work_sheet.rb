398cafbde03b7390d5489dc7ad97e28e8193ccb8
parasquid <tristan.gomez@gmail.com>
2015-12-10 02:14:16 +0800

    retrieve a list of rows from the document
    the document is represented by a hash with the header as the key
    and the cell contents as the value.
    
    the hash keys are strings but they're frozen to aid in gc

--- lib/gsheets/work_sheet.rb
+++ lib/gsheets/work_sheet.rb
@@ -10,13 +10,32 @@ module Gsheets
       cells_first_row["feed"]["entry"].map {|e| e["content"]["$t"]}
     end
 
+    def rows
+      # binding.pry
+      list = worksheet_as_list["feed"]["entry"].map { |entry| entry["content"]["$t"] }
+      result = []
+      list.each do |l|
+        row = {}
+        l.split(",").each do |elem|
+          e = elem.split(":")
+          row[e.first.strip.freeze] = e.last.strip.freeze
+        end
+        result << row
+      end
+      result
+    end
+
     private
 
     def cells_first_row
-      JSON.parse(@session.get_cells(id: @id, grid_id: @grid_id, options: header_row).body)
+      JSON.parse(@session.get_cells(id: @id, grid_id: @grid_id, options: header_row_options).body)
     end
 
-    def header_row
+    def worksheet_as_list
+      JSON.parse(@session.get_list(id: @id, grid_id: @grid_id).body)
+    end
+
+    def header_row_options
       ["min-row=1", "max-row=1"]
     end
   end
